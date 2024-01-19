f38bc57852da176fedbc098b186deeb80964cf19
Nofre417 <nofrevideo@gmail.com>
2024-01-19 09:35:25 +0100

    Some updates

---
+++ WorkWithBenchmark.NET/Program.cs
@@ -0,0 +1,11 @@
+﻿using WorkWithBenchmark;
+using BenchmarkDotNet.Running;
+
+using static System.Console;
+using System;
+using WorkWithBenchmark.NET;
+
+WriteLine("_BENCHMARKING_");
+
+//Run Benchmark.NET
+BenchmarkRunner.Run<StringBenchmarking>();
---
+++ WorkWithBenchmark.NET/StringBenchmarking.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace WorkWithBenchmark.NET
+{
+    public class StringBenchmarking
+    {
+        int[] numbers;
+
+        public StringBenchmarking()
+        {
+            numbers = Enumerable.Range(start: 1, count: 20).ToArray();
+        }
+
+        [Benchmark(Baseline = true)]
+        public string StringConcatenationTest()
+        {
+            string str = string.Empty;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                str += numbers[i] + ", ";
+            }
+            return str;
+        }
+
+        [Benchmark]
+        public string StringBuilderTest()
+        {
+            System.Text.StringBuilder sb = new();
+            for(int i = 0; i < numbers.Length; i++) 
+            {
+                sb.Append(numbers[i]);
+                sb.Append(", ");
+            }
+            return sb.ToString();
+        }
+    }
+}
